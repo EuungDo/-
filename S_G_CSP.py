8278b46434ed7ae4dfe6c41b26f18cc82effee18
Kim_Eung_Do_HBNU <159219230+EuungDo@users.noreply.github.com>
2024-08-02 12:57:07 +0900

    Add files via upload

--- S_G_CSP.py
+++ S_G_CSP.py
@@ -1,39 +1,68 @@
 import time
 import socket
-import serial
 import threading
 import struct
 from queue import Queue
+from pyrf24 import RF24, RF24_PA_LOW, RF24_DRIVER
 
 ###############################################################
-# Ground to Satellite Communication Support Program
-# 지상국 통신 보조 프로그램
-# 송신과 수신을 각각 담당하는 두개의 아두이노를 이용해 데이터 송/수신
+# Satellite to Ground Communication Support Program
+# 위성 통신 보조 프로그램
+# CCSDS 패킷에 따라서 패킷 조립 및 분해
+# UDP/IP 통신과 nrf 통신을 하나의 프로그램으로 통제해야 함.
 ###############################################################
 
 #########################################################################################
 #
-# 시리얼 통신 및 큐(Queue) 설정
+# NRF 통신 및 큐(Queue) 설정
 #
 #########################################################################################
 
-# 송신용 아두이노 시리얼 포트 설정
-ser_tx = serial.Serial(
-    port='/dev/ttyACM1',  # 적절한 포트로 변경
-    baudrate=115200,
-    timeout=1
-)
+# 수신용 라디오 핀 설정 
+CSN_PIN_1 = 0  # SPI0 CE0 -> spidev 0.0
+if RF24_DRIVER == 'MRAA':
+    CE_PIN_1 = 15
+elif RF24_DRIVER == 'wiringPi':
+    CE_PIN_1 = 3
+else:
+    CE_PIN_1 = 22
 
-# 수신용 아두이노 시리얼 포트 설정
-ser_rx = serial.Serial(
-    port='/dev/ttyACM0',  # 적절한 포트로 변경
-    baudrate=115200,
-    timeout=1
-)
+radio_rx = RF24(CE_PIN_1, CSN_PIN_1)
 
-# UDP/IP 큐(Queue) 설정
-send_queue = Queue()
+# 송신용 라디오 핀 설정
+CSN_PIN_2 = 12  # SPI1 CE2 -> spidev1.2
+if RF24_DRIVER == 'MRAA':
+    CE_PIN_2 = 32
+elif RF24_DRIVER == 'wiringPi':
+    CE_PIN_2 = 26
+else:
+    CE_PIN_2 = 12
+
+radio_tx = RF24(CE_PIN_2, CSN_PIN_2)
+
+if not radio_rx.begin():
+    raise RuntimeError("radio_rx hardware is not responding")
+
+if not radio_tx.begin():
+    raise RuntimeError("radio_tx hardware is not responding")
+
+address = [b"1Node", b"2Node"]
+
 receive_queue = Queue()
+send_queue = Queue()
+
+# radio_rx Hardware Setting
+radio_rx.setPALevel(RF24_PA_LOW)
+radio_rx.openReadingPipe(1, address[0])
+radio_rx.setChannel(0)
+radio_rx.payloadSize = 32  # Set the payload size to the maximum for simplicity
+radio_rx.startListening()
+
+# radio_tx Hardware Setting
+radio_tx.setPALevel(RF24_PA_LOW)
+radio_tx.openWritingPipe(address[1])
+radio_tx.setChannel(100)
+radio_tx.payloadSize = 32
 
 #########################################################################################
 #
@@ -44,7 +73,7 @@ receive_queue = Queue()
 # UDP/IP 수신
 def udp_receiver():
     udp_ip = "127.0.0.1"
-    udp_port = 1234
+    udp_port = 1235
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((udp_ip, udp_port))
@@ -60,7 +89,7 @@ def udp_receiver():
 # UDP/IP 송신
 def udp_sender():
     udp_ip = "127.0.0.1"
-    udp_port = 1235
+    udp_port = 1234
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         
@@ -70,39 +99,39 @@ def udp_sender():
             sock.sendto(message, (udp_ip, udp_port))
             print(f"Sent UDP message to {udp_ip}:{udp_port}\n {message}")
 
-# 시리얼 송신
-def send_to_arduino():
+# NRF24L01 송신
+def nrf24_sender():
     while True:
         if not send_queue.empty():
-            message = send_queue.get()
-            escaped_message = escape_data(message)
-            if ser_tx.is_open:
-                ser_tx.write(escaped_message)
-                print(f"Message sent to Arduino on port {ser_tx.port}\n")
+            chunk = send_queue.get()
+            if radio_tx.write(chunk):
+                print(f"Sent chunk: {chunk.hex()}")
+            else:
+                print("Sending failed")
 
-# 시리얼 수신 및 데이터 처리
-def read_from_arduino():
+# NRF24L01 수신 및 데이터 처리
+def nrf24_receiver():
     received_data = b''
     expected_length = None
 
     while True:
-        if ser_rx.is_open and ser_rx.in_waiting > 0:
-            incoming_message = ser_rx.read(32)
+        if radio_rx.available():
+            length = radio_rx.getDynamicPayloadSize()
+            incoming_message = radio_rx.read(length)
             received_data += incoming_message
+            print(f"Received chunk: {incoming_message.hex()}")
 
             while len(received_data) >= 6:  # 최소한 헤더 크기만큼 데이터가 있어야 함
-                unescaped_data = unescape_data(received_data)
-
                 if expected_length is None:
                     # 헤더를 읽고 전체 패킷 길이 계산
-                    header = unescaped_data[:6]
+                    header = received_data[:6]
                     header_info = parse_primary_header(header)
                     total_length = header_info["Packet Length"] + 1 + 6  # 패킷 길이 + 1 + 헤더 크기 (6바이트)
                     expected_length = total_length
 
                 # 수신된 데이터의 총 길이가 예상된 길이와 같거나 클 때
-                if len(unescaped_data) >= expected_length:
-                    packet = unescaped_data[:expected_length]  # 패킷을 분리
+                if len(received_data) >= expected_length:
+                    packet = received_data[:expected_length]  # 패킷을 분리
                     print(f"Reassembled Data: {packet.hex()}")
                     receive_queue.put(packet)  # UDP 송신 큐에 추가
                     received_data = received_data[expected_length:]  # 처리한 패킷을 제거
@@ -110,7 +139,15 @@ def read_from_arduino():
                 else:
                     break
 
-# CCSDS 페킷 헤더 분석
+# CCSDS 패킷 분해
+def split_data(data, chunk_size=32):
+    chunks = []
+    for i in range(0, len(data), chunk_size):
+        chunk = data[i:i + chunk_size]
+        chunks.append(chunk)
+    return chunks
+
+# CCSDS 패킷 헤더 분석
 def parse_primary_header(header):
     version_type_secflag_apid = struct.unpack(">H", header[:2])[0]
     seq_flags_seq_count = struct.unpack(">H", header[2:4])[0]
@@ -134,26 +171,6 @@ def parse_primary_header(header):
         "Packet Length": packet_length
     }
 
-# 시리얼 통신시 0x00 바이트인 경우 통신 중단됨.
-# 이를 방지하기 위한 데이터 변조 실시
-def escape_data(data):
-    escaped = data.replace(b'\xFF', b'\xFF\xFF').replace(b'\x00', b'\xFF\x00')
-    return escaped
-
-def unescape_data(data):
-    unescaped = data.replace(b'\xFF\x00', b'\x00').replace(b'\xFF\xFF', b'\xFF')
-    return unescaped
-
-# CCSDS 패킷 분해
-# 헤더는 처음 6바이트까지
-# 32 바이트씩 쪼개서 데이터 저장
-def split_data(data, chunk_size=32):
-    chunks = []
-    for i in range(0, len(data), chunk_size):
-        chunk = data[i:i + chunk_size]
-        chunks.append(chunk)
-    return chunks
-
 #########################################################################################
 #
 # 메인 함수 작성
@@ -162,20 +179,20 @@ def split_data(data, chunk_size=32):
 
 def main():
     # 스레드 시작
+    nrf24_recv_thread = threading.Thread(target=nrf24_receiver)
     udp_recv_thread = threading.Thread(target=udp_receiver)
     udp_send_thread = threading.Thread(target=udp_sender)
-    serial_send_thread = threading.Thread(target=send_to_arduino)
-    serial_recv_thread = threading.Thread(target=read_from_arduino)
+    nrf24_send_thread = threading.Thread(target=nrf24_sender)
     
+    nrf24_recv_thread.start()
     udp_recv_thread.start()
     udp_send_thread.start()
-    serial_send_thread.start()
-    serial_recv_thread.start()
+    nrf24_send_thread.start()
 
+    nrf24_recv_thread.join()
     udp_recv_thread.join()
     udp_send_thread.join()
-    serial_send_thread.join()
-    serial_recv_thread.join()
+    nrf24_send_thread.join()
 
 if __name__ == "__main__":
     main()
